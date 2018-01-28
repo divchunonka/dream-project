ea5c3ca52008cedfb1a85401c2848e9db9bdea23
Vitaliy Moskalyuk <vitaliy.moskalyuk@zoomdata.com>
2018-01-28 22:28:58 +0200

    greated base gradle+spring project with basic controller

---
+++ dream-server/src/main/java/com/project/dream/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.project.dream.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String login() {
+        return "login";
+    }
+}
